a17d9e3c057723b30c309cb2ee34844fc463e835
David Lyons <david.scott.lyons@gmail.com>
2023-11-10 22:01:39 -0800

    update deps, suspense loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from 'react';
 import * as THREE from 'three';
 import { Canvas } from '@react-three/fiber';
 import { Environment, OrbitControls } from '@react-three/drei';
 import CustomObject from './components/CustomObject';
 import { Box } from './components/Box';
 import { Suzanne } from './components/Suzanne';
+import { Loader } from './components/Loader';
 
 // https://docs.pmnd.rs/react-three-fiber/tutorials/typescript
 // TS Starter https://codesandbox.io/s/brnsm
@@ -26,22 +28,24 @@ export default function App() {
         position: [2, 2, 7],
       }}
     >
-      <OrbitControls enableDamping={false} />
-      <gridHelper />
+      <Suspense fallback={<Loader />}>
+        <OrbitControls enableDamping={false} />
+        <gridHelper />
 
-      {/* <ambientLight intensity={0.5} />
+        {/* <ambientLight intensity={0.5} />
       <directionalLight position={[1, 2, 3]} intensity={1.5} /> */}
 
-      <Environment preset="sunset" background />
+        <Environment preset="city" background resolution={100} />
 
-      <group>
-        <Box position={[-2.2, 0, 0]} />
-        <Box position={[2.2, 0, 0]} />
-      </group>
+        <group>
+          <Box position={[-2.2, 0, 0]} />
+          <Box position={[2.2, 0, 0]} />
+        </group>
 
-      <CustomObject position-x={-5} />
+        <CustomObject position-x={-5} />
 
-      <Suzanne />
+        <Suzanne />
+      </Suspense>
     </Canvas>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env;
-
 export default defineConfig({
-  plugins: [react()],
   root: 'src/',
   publicDir: '../public/',
   base: './',
+  plugins: [react()],
   server: {
     host: true,
-    open: !isCodeSandbox, // Open if it's not a CodeSandbox
+    open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env), // Open if it's not a CodeSandbox
   },
   build: {
     outDir: '../dist',
